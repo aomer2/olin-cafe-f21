9bac16f58a7fb068f19b48abe7d8b7807a58581c
aomer <aomer@olin.edu>
2021-10-12 19:32:40 -0400

    Update led array with correct n=3 outputs.

--- 01_game_of_life/led_array_driver.sv
+++ 01_game_of_life/led_array_driver.sv
@@ -30,35 +30,53 @@ module led_array_driver(ena, x, cells, rows, cols);
   decoder_3_to_8 COL_DECODER(ena, x, x_decoded);
 
   always_comb begin : led_array_driver
-    rows[0] = ~((x_decoded[4] & cells[4]) |
-             (x_decoded[3] & cells[3]) |
-             (x_decoded[2] & cells[2]) |
-             (x_decoded[1] & cells[1]) |
-             (x_decoded[0] & cells[0]));
 
-    rows[1] = ~((x_decoded[4] & cells[9]) |
-             (x_decoded[3] & cells[8]) |
-             (x_decoded[2] & cells[7]) |
-             (x_decoded[1] & cells[6]) |
-             (x_decoded[0] & cells[5]));
+    rows = 0;
+    if (N == 3) begin
+      rows[2] = ~((x_decoded[2] & cells[2]) |
+                  (x_decoded[1] & cells[1]) |
+                  (x_decoded[0] & cells[0]));
 
-    rows[2] = ~((x_decoded[4] & cells[14]) |
-             (x_decoded[3] & cells[13]) |
-             (x_decoded[2] & cells[12]) |
-             (x_decoded[1] & cells[11]) |
-             (x_decoded[0] & cells[10]));
+      rows[1] = ~((x_decoded[2] & cells[5]) |
+                  (x_decoded[1] & cells[4]) |
+                  (x_decoded[0] & cells[3]));
 
-    rows[3] = ~((x_decoded[4] & cells[19]) |
-             (x_decoded[3] & cells[18]) |
-             (x_decoded[2] & cells[17]) |
-             (x_decoded[1] & cells[16]) |
-             (x_decoded[0] & cells[15]));
+      rows[0] = ~((x_decoded[2] & cells[8]) |
+                  (x_decoded[1] & cells[7]) |
+                  (x_decoded[0] & cells[6]));
 
-    rows[4] = ~((x_decoded[4] & cells[24]) |
-             (x_decoded[3] & cells[23]) |
-             (x_decoded[2] & cells[22]) |
-             (x_decoded[1] & cells[21]) |
-             (x_decoded[0] & cells[20]));
+    end
+    if (N == 5) begin
+      rows[4] = ~((x_decoded[4] & cells[4]) |
+              (x_decoded[3] & cells[3]) |
+              (x_decoded[2] & cells[2]) |
+              (x_decoded[1] & cells[1]) |
+              (x_decoded[0] & cells[0]));
+
+      rows[3] = ~((x_decoded[4] & cells[9]) |
+              (x_decoded[3] & cells[8]) |
+              (x_decoded[2] & cells[7]) |
+              (x_decoded[1] & cells[6]) |
+              (x_decoded[0] & cells[5]));
+
+      rows[2] = ~((x_decoded[4] & cells[14]) |
+              (x_decoded[3] & cells[13]) |
+              (x_decoded[2] & cells[12]) |
+              (x_decoded[1] & cells[11]) |
+              (x_decoded[0] & cells[10]));
+
+      rows[1] = ~((x_decoded[4] & cells[19]) |
+              (x_decoded[3] & cells[18]) |
+              (x_decoded[2] & cells[17]) |
+              (x_decoded[1] & cells[16]) |
+              (x_decoded[0] & cells[15]));
+
+      rows[0] = ~((x_decoded[4] & cells[24]) |
+              (x_decoded[3] & cells[23]) |
+              (x_decoded[2] & cells[22]) |
+              (x_decoded[1] & cells[21]) |
+              (x_decoded[0] & cells[20]));
+    end
 
     cols[0] = x_decoded[0];
     cols[1] = x_decoded[1];
